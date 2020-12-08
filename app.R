335af0c30b7a38b461756ca489477d5ffec172ef
Kacper Perz <kicper2000@gmail.com>
2020-12-08 18:30:51 +0100

    Basic graph and rcpp testing

--- app.R
+++ app.R
@@ -23,7 +23,7 @@ e_sum <- 0
 R <- rep(0,sim_cycles)
 u <- rep(0,sim_cycles)
 e <- rep(0,sim_cycles)
-temp <- rep(0,sim_cycles)
+
 
 cppFunction('void f(int sim_cycles,NumericVector &e, NumericVector &temp, NumericVector &u, const double T, const double& kp, const double& Tp, const double& Ti, int T0, const double& u_max, const double& u_min, double& e_sum, const double& Td){
     for(int i=0 ; i<sim_cycles ; ++i) {
@@ -44,35 +44,67 @@ cppFunction('void f2(int sim_cycles, NumericVector &temp) {
 
 ui <- fluidPage(
   sliderInput(inputId = "temp0",
-    label = "Choose a value",
+    label = "Temp0.",
     value = 2, min = 0, max = 10),
   sliderInput(inputId = "temp_dest",
-    label = "Choose a value",
+    label = "Temp_dest.",
     value = 20, min = 0, max = 100),
   sliderInput(inputId = "U",
-    label = "Choose a value",
+    label = "Napiecie.",
     value = 250, min = 0, max = 250),
   plotOutput('val'),
   textOutput('val2')
   
 )
 
-
+cppFunction('void f3(NumericVector &a,NumericVector &b) {
+  int n = a.size();
+  for(int i=0; i<n ; ++i) {
+  a[i] = i;
+  b[i] = 9-i;
+  }
+}')
 
 server <- function(input, output) {
   
-  temp_dest <- isolate({ input$temp_dest })
-  temp[1] <- isolate({ input$temp0 })
-  tddd <- temp[1]
+  temp <- reactive({ c(input$temp, rep(0,sim_cycles)) })
+  temp_dest <- reactive({ input$temp_dest })
+  
   #f2(sim_cycles, temp)
-  f(sim_cycles, e, temp, u, temp_dest, kp, Tp, Ti, tddd, u_max, u_min, e_sum, Td)
+  #reactive({f(sim_cycles, e, temp, u, temp_dest, kp, Tp, Ti, input$temp0, u_max, u_min, e_sum, Td)})
   
   
+  
+  obliczenia <- reactive({
+    temp_l <- temp()
+    temp_dest_l <- temp_dest()
+    temp0_l <- input$temp0
+    f(sim_cycles, e, temp_l, u, temp_dest_l, kp, Tp, Ti, temp0_l, u_max, u_min, e_sum, Td)
+    temp_l
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  a <- reactive({ c(1,2,3) })
+  b <- reactive({ c(1,2,3) })
+  
+  reactive({f3(a,b)})
+  lala <- reactive({
+    f3_a <- a()
+    f3_b <- b()
+    f3(f3_a,f3_b)
+    c(f3_a,f3_b)
+  })
   output$val <- renderPlot({
-    plot(time_step,temp)
+    plot(time_step,obliczenia())
     })
   output$val2 <- renderText({
-    temp
+    c(length(obliczenia()), length(time_step))
   })
 }
 shinyApp(ui = ui, server = server)
